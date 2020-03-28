2b180b9c67d9a2291c2e78e4d6cd272237a35b2c
Qiguang Chu <chuqiguang@gmail.com>
2020-03-28 01:04:53 -0400

    finish

--- Part 3/Project.py
+++ Part 3/Project.py
@@ -10,9 +10,9 @@ class Inspector1:
         self.number = 0
 
     def generate(self):
-        """Generate one time needed to inspect a component C1"""     
+        """Generate one time needed to inspect a component C1"""
         self.number += 1 if self.number + 1 <= len(self.sp) else 0
-        return self.sp[self.number-1]
+        return self.sp[self.number - 1]
 
     def whichtosend(self, W1, W2, W3):
         """Decide which workstation to send the component"""
@@ -39,7 +39,9 @@ class Workstation1:
     def generate(self):
         """Generate one time needed to finish a product"""
         self.number += 1 if self.number + 1 < len(self.ws) else 0
-        return self.ws[self.number-1]
+        self.buffer['C1'] -= 1
+        self.isWorking = True
+        return self.ws[self.number - 1]
 
     def canWork(self):
         """If the Workstation can work now"""
@@ -64,13 +66,13 @@ class Inspector2:
         """Generate one time needed to inspect a component C2"""
         self.number22 += 1 if self.number22 + 1 <= len(self.sp22) else 0
         self.which = True
-        return self.sp22[self.number22-1]
+        return self.sp22[self.number22 - 1]
 
     def generate23(self):
         """Generate one time needed to inspect a component C3"""
         self.number23 += 1 if self.number23 + 1 <= len(self.sp23) else 0
         self.which = False
-        return self.sp23[self.number23-1]
+        return self.sp23[self.number23 - 1]
 
     def whichtosend(self, W1, W2, W3):
         """Decide which Workstation to send"""
@@ -96,7 +98,8 @@ class Workstation2:
         self.number += 1 if self.number + 1 < len(self.ws) else 0
         self.buffer["C1"] -= 1
         self.buffer["C2"] -= 1
-        return self.ws[self.number-1]
+        self.isWorking = True
+        return self.ws[self.number - 1]
 
     def canWork(self):
         """If the Workstation can work now"""
@@ -115,10 +118,11 @@ class Workstation3:
 
     def generate(self):
         """Generate one time needed to finish a product P3"""
-        m = self.number
-        if self.number + 1 < len(self.ws):
-            self.number += 1
-        return self.ws[m]
+        self.number += 1 if self.number + 1 < len(self.ws) else 0
+        self.buffer['C1'] -= 1
+        self.buffer['C3'] -= 1
+        self.isWorking = True
+        return self.ws[self.number - 1]
 
     def canWork(self):
         """If the work station can work now"""
@@ -135,9 +139,9 @@ if __name__ == '__main__':
     idle_start = 0
     idle_end = 0
 
-    event_list = []  # [time, objective, component, activity]
-    event_list += [[World_time + I1.generate(), I1.whichtosend(W1, W2, W3), I1.component(), 'receive']]
-    event_list += [[World_time + I2.generate(), I2.whichtosend(W1, W2, W3), I2.component(), 'receive']]
+    event_list = [[World_time + I1.generate(), I1.whichtosend(W1, W2, W3), I1.component(), 'receive'],
+                  [World_time + I2.generate(), I2.whichtosend(W1, W2, W3), I2.component(), 'receive']]
+    # format: [time, objective, component, activity]
     # initialize the environment
 
     while event_list:
@@ -149,41 +153,35 @@ if __name__ == '__main__':
 
         if event[-1] == 'receive':
             if event[1].buffer[event[2]] <= 1:
-                # if the buffer has room to receive the component
+                # if the buffer has room to admit the component
                 event[1].buffer[event[2]] += 1
                 if not event[1].isWorking and event[1].canWork():
                     # the work station has ingredients to produce
-                    for item in event[1].buffer:
-                        event[1].buffer[item] -= 1
                     event_list.append([World_time + event[1].generate(), event[1], '', 'output'])
-                    event[1].isWorking = True
                     event[1].idle_time += World_time - event[1].start_idle
-                if generator == I1 and generator.number >= 299:
+                if generator == I1 and generator.number >= 300:
                     continue
-                if generator == I2 and (generator.number22 >= 299 or generator.number23 >= 299):
+                if generator == I2 and (generator.number22 >= 300 or generator.number23 >= 300):
                     continue
                 event_list.append(
                     [World_time + generator.generate(), generator.whichtosend(W1, W2, W3), generator.component(),
                      'receive'])
             else:
-                # no room for this component, we have to wait for room to settle down
+                # no room for this component, it has to wait for room to settle down
                 k = False
                 for eve in event_list:
                     if eve[-1] == 'output' and eve[1] == event[1]:
-                        event_list.append([eve[0] + 0.001, eve[1], event[2], 'receive'])
+                        event_list.append([eve[0] + 0.001, event[1], event[2], 'receive'])
                         k = True
                         break
-                if not k and not len(event_list):
+                if not k and len(event_list):
                     event_list.append([event_list[0][0] + 0.001, event[1], event[2], 'receive'])
         elif event[-1] == 'output':
             # output the product
             event[1].isWorking = False
             event[1].start_idle = World_time
             if event[1].canWork():
-                for item in event[1].buffer:
-                    event[1].buffer[item] -= 1
                 event_list.append([World_time + event[1].generate(), event[1], '', 'output'])
-                event[1].isWorking = True
                 event[1].idle_time += World_time - event[1].start_idle
 
     print(World_time, W1.number, W2.number, W3.number, W1.idle_time, W2.idle_time, W3.idle_time)
