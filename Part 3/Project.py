7b24eb274289196a6179c4033aeb765f2f778d41
Qiguang Chu <chuqiguang@gmail.com>
2020-03-29 15:40:58 -0400

    Correct the diagram

--- Part 3/Project.py
+++ Part 3/Project.py
@@ -29,14 +29,14 @@ class Inspector1:
 
 class Workstation1:
     def __init__(self):
+        self.__name__ = 'W1'
         self.buffer = {"C1": 0}  # number of components in the buffer
-        self.deal_time = 0  # accumulative time
+        self.dealTime = 0  # accumulative time
         self.ws = np.loadtxt('ws1.txt')  # number of outputs totally
         self.number = 0  # number of outputs totally
         self.isWorking = False  # if the workstation is in working condition
-        self.idle_time = 0
-        self.start_idle = 0
-        self.__name__ = 'W1'
+        self.idleTime = 0
+        self.startIdle = 0
 
     def generate(self):
         """Generate one time needed to finish a product"""
@@ -87,14 +87,14 @@ class Inspector2:
 
 class Workstation2:
     def __init__(self):
+        self.__name__ = 'W2'
         self.buffer = {"C1": 0, "C2": 0}
         self.deal_time = 0  # accumulative time
         self.ws = np.loadtxt('ws2.txt')
         self.number = 0  # number of outputs totally
         self.isWorking = False
-        self.idle_time = 0
-        self.start_idle = 0
-        self.__name__ = 'W2'
+        self.idleTime = 0
+        self.startIdle = 0
 
     def generate(self):
         """Generate one time needed to finish a product P2"""
@@ -111,14 +111,14 @@ class Workstation2:
 
 class Workstation3:
     def __init__(self):
+        self.__name__ = 'W3'
         self.buffer = {"C1": 0, "C3": 0}
         self.deal_time = 0  # accumulative time
         self.ws = np.loadtxt('ws3.txt')
         self.number = 0  # number of outputs totally
         self.isWorking = False
-        self.idle_time = 0
-        self.start_idle = 0
-        self.__name__ = 'W3'
+        self.idleTime = 0
+        self.startIdle = 0
 
     def generate(self):
         """Generate one time needed to finish a product P3"""
@@ -132,6 +132,7 @@ class Workstation3:
         """If the work station can work now"""
         return self.buffer["C1"] > 0 and self.buffer["C3"] > 0
 
+
 def queue_diagram(Buffer, name):
     res = [buffer for buffer in Buffer if buffer[-1] == name]
 
@@ -140,81 +141,81 @@ def queue_diagram(Buffer, name):
     y = [a[1] for a in res]
 
     total = 0
-    for i in range(len(x)-1):
-        total+=(x[i+1]-x[i])*y[i]
+    for i in range(len(x) - 1):
+        total += (x[i + 1] - x[i]) * y[i]
 
-    average = [total/x[-1] for _ in range(len(x))]
+    average = [total / x[-1] for _ in range(len(x))]
+    print(average[0], 189 / x[-1], total / 189)
 
     plt.step(x, y, where='post')
     plt.plot(x, average, 'r')
     print(len(res))
-    plt.show()
+    # plt.show()
 
 
 if __name__ == '__main__':
-    World_time = 0
+    worldTime = 0
     I1 = Inspector1()
     I2 = Inspector2()
     W1 = Workstation1()
     W2 = Workstation2()
     W3 = Workstation3()
-    idle_start = 0
-    idle_end = 0
+    idleStart = 0
+    idleEnd = 0
 
     Buffer = [[0, W1.buffer['C1'], 'W1_C1'], [0, W2.buffer['C1'], 'W2_C1'],
               [0, W2.buffer['C2'], 'W2_C2'], [0, W3.buffer['C1'], 'W3_C1'],
               [0, W3.buffer['C3'], 'W3_C3']]
     # Buffer = [World_time, which_buffer, buffer_name]
 
-    event_list = [[World_time + I1.generate(), I1.whichtosend(W1, W2, W3), I1.component(), 'receive'],
-                  [World_time + I2.generate(), I2.whichtosend(W1, W2, W3), I2.component(), 'receive']]
+    eventList = [[worldTime + I1.generate(), I1.whichtosend(W1, W2, W3), I1.component(), 'receive'],
+                 [worldTime + I2.generate(), I2.whichtosend(W1, W2, W3), I2.component(), 'receive']]
     # format: [time, objective, component, activity]
     # initialize the environment
 
-    while event_list:
-        event_list.sort()
-        event = event_list.pop(0)
+    while eventList:
+        eventList.sort()
+        event = eventList.pop(0)
         # take out the nearest event from event list
-        World_time = event[0]
+        worldTime = event[0]
         generator = I1 if event[2] == 'C1' else I2
 
         if event[-1] == 'receive':
             if event[1].buffer[event[2]] <= 1:
                 # if the buffer has room to admit the component
                 event[1].buffer[event[2]] += 1
-                Buffer.append([World_time, event[1].buffer[event[2]], event[1].__name__ + '_' + event[2]])
+                Buffer.append([worldTime, event[1].buffer[event[2]], event[1].__name__ + '_' + event[2]])
                 if not event[1].isWorking and event[1].canWork():
                     # the work station has ingredients to produce
-                    time = World_time + event[1].generate()
-                    event_list.append([time, event[1], '', 'output'])
+                    time = worldTime + event[1].generate()
+                    eventList.append([time, event[1], '', 'output'])
                     Buffer.append([time, event[1].buffer[event[2]], event[1].__name__ + '_' + event[2]])
-                    event[1].idle_time += World_time - event[1].start_idle
+                    event[1].idleTime += worldTime - event[1].startIdle
                 if generator == I1 and generator.number >= 300:
                     continue
                 if generator == I2 and (generator.number22 >= 300 or generator.number23 >= 300):
                     continue
-                event_list.append(
-                    [World_time + generator.generate(), generator.whichtosend(W1, W2, W3), generator.component(),
+                eventList.append(
+                    [worldTime + generator.generate(), generator.whichtosend(W1, W2, W3), generator.component(),
                      'receive'])
             else:
                 # no room for this component, it has to wait for room to settle down
                 k = False
-                for eve in event_list:
+                for eve in eventList:
                     if eve[-1] == 'output' and eve[1] == event[1]:
-                        event_list.append([eve[0] + 0.001, event[1], event[2], 'receive'])
+                        eventList.append([eve[0] + 0.001, event[1], event[2], 'receive'])
                         k = True
                         break
-                if not k and len(event_list):
-                    event_list.append([event_list[0][0] + 0.001, event[1], event[2], 'receive'])
+                if not k and len(eventList):
+                    eventList.append([eventList[0][0] + 0.001, event[1], event[2], 'receive'])
         elif event[-1] == 'output':
             # output the product
             event[1].isWorking = False
-            event[1].start_idle = World_time
+            event[1].startIdle = worldTime
             if event[1].canWork():
-                event_list.append([World_time + event[1].generate(), event[1], '', 'output'])
-                event[1].idle_time += World_time - event[1].start_idle
+                eventList.append([worldTime + event[1].generate(), event[1], '', 'output'])
+                event[1].idleTime += worldTime - event[1].startIdle
 
-    print(World_time, W1.number, W2.number, W3.number, W1.idle_time, W2.idle_time, W3.idle_time)
+    print(worldTime, W1.number, W2.number, W3.number, W1.idleTime, W2.idleTime, W3.idleTime)
+    print(np.mean(W1.ws))
     queue_diagram(Buffer, 'W1_C1')
-
-
